de313e204e4c14bcee439a73f5cf57ef618f5a77
Yeluzii <2317413623@qq.com>
2025-06-10 19:59:06 +0800

    feat: 后端项目初始化

---
+++ iot-dashboard-api/src/main/java/fun/ychen/iot/IotDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package fun.ychen.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotDashboardApiApplication.class, args);
+    }
+
+}
