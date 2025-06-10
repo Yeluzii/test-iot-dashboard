94faf254a8db13491de27ea44211f94eaffe10ff
Yeluzii <2317413623@qq.com>
2025-06-10 19:59:06 +0800

    feat: 后端项目初始化

---
+++ iot-dashboard-api/src/test/java/fun/ychen/iot/IotDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.ychen.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDashboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
